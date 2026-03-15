03dc10be79dc646c9c02d958a19e3cb4cce6c1db
Raghav Dhingra <raghavdhingra479@gmail.com>
2026-03-16 05:11:45 +0530

    fix deployment

--- Backend/index.js
+++ Backend/index.js
@@ -2,28 +2,24 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 import connectDB from "./utils/db.js"
+import path from "path"
 
 import userRouter from "./routes/user.route.js"
 import CompanyRouter from "./routes/company.route.js"
 import JobRouter from "./routes/job.route.js"
 import ApplicationRouter from "./routes/application.route.js"
-import path from "path"
-
 
 import dotenv from "dotenv"
-
 dotenv.config()
+
 const app = express()
-
-
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
-const corsOptions = {
 
-    origin:"http://localhost:5173",
-    
+const corsOptions = {
+    origin:true,
     credentials:true
 }
 
@@ -34,22 +30,17 @@ app.use("/api/v1/company",CompanyRouter)
 app.use("/api/v1/job",JobRouter)
 app.use("/api/v1/application",ApplicationRouter)
 
-
 const __dirname = path.resolve()
 
 app.use(express.static(path.join(__dirname,"../Frontend/vite-project/dist")))
 
-app.get("/*",(req,res)=>{
+app.use((req,res)=>{
    res.sendFile(path.join(__dirname,"../Frontend/vite-project/dist/index.html"))
 })
 
-
-
 const port = process.env.PORT || 3000
-app.listen(port,()=>
-{   
+
+app.listen(port,()=>{
     connectDB()
     console.log(`Listening at port : ${port}`)
 })
-
-
