18ad4aff55ed6562df9830c15a840a39df4a4737
Raghav Dhingra <raghavdhingra479@gmail.com>
2026-03-16 05:35:10 +0530

    fix api endpoints for deployment

--- Frontend/vite-project/src/components/auth/Signup.jsx
+++ Frontend/vite-project/src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
